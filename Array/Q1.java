477ce0a55ede3903a1e4edea855c110328711b65
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-16 16:06:19 +0530

    array

---
+++ Array/Q1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Q1 {
+    public static void rotateRight(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n; // Handle cases where k > n
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        rotateRight(arr, 2);
+        System.out.println(Arrays.toString(arr)); 
+    }
+}
+
+
