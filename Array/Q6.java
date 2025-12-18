9e5a428226c3022b46c904c2a5613497b9b741f0
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-18 20:18:18 +0530

    negative element

---
+++ Array/Q6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array Elements : ");
+        for(int i=0;i<n;i++)
+        arr[i] = sc.nextInt();  
+        System.out.print("Negative Elements Are : ");
+        for(int i=0;i<n;i++)
+        if(arr[i]<0)  System.out.print(arr[i]+" ");  
+        sc.close();    
+    }
+}
+
