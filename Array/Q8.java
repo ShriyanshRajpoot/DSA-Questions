b341523df7199e63994765a38dd27828259d1eef
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-19 15:52:42 +0530

    second max element

---
+++ Array/Q8.java
@@ -0,0 +1,17 @@
+public class Q8 {
+    public static void main(String[] args) {
+        int[] arr = {2,3,6,2,8,9};
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max) max=arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>smax && arr[i]!=max) smax=arr[i];
+        }
+        System.out.println(max);
+        System.out.println(smax);
+    }
+}
+
