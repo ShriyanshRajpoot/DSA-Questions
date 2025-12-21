59b4535a8b9d55838663bdd5efc955b3819a0eea
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-21 17:58:04 +0530

    sum of elements

---
+++ Array/Q11.java
@@ -0,0 +1,11 @@
+public class Q11 {
+    public static void main(String[] args) {
+        int[] arr = {4,6,8,9,2};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
+
