26bd8bc9e33104af24f90b7918784550b786eb87
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-22 21:00:15 +0530

    powerloop

---
+++ Q13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q13 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int pow = 1;
+        for(int i=1;i<=b;i++){
+            pow *= a;
+        }
+        System.out.println(a+" raised to the power "+b+" is "+pow);
+        sc.close();
+    }
+}
+
+
