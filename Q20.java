370fd56ab0d85d80bad3e5ed5741d9159bf2c8b7
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-26 21:17:03 +0530

    flodys triangle

---
+++ Q20.java
@@ -0,0 +1,19 @@
+// flodys triangle
+import java.util.Scanner;
+
+public class Q20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(a++ + " ");
+            }
+                  System.out.println();
+                  sc.close();
+        }
+
+    }
+}
+
