b0db47e40f0fef5bf079c6a5fa19ac5288289124
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-07 19:47:11 +0530

    selection sort

---
+++ Q37.java
@@ -0,0 +1,28 @@
+public class Q37 {
+    public static void main(String[] args) {
+        int[] arr = {5,-2,6,7,2,0,7,2};
+        int n = arr.length;
+        for (int ele : arr) {
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        for(int i=0;i<n-1;i++){
+            int min = Integer.MAX_VALUE;
+            int mindx = -1;
+            for(int j=i;j<n;j++){
+                if(arr[j]<min){
+                    min = arr[j];
+                    mindx = j;
+                }     
+            }
+
+            int temp  = arr[i];
+            arr[i] = arr[mindx];
+            arr[mindx]  = temp;
+        }
+        for (int k : arr) {
+            System.out.print(k+" ");
+        }
+    }
+}
+
