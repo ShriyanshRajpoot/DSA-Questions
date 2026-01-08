5492c1d0db168a6f91a40ed3ae9e96a88958224a
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-08 20:47:54 +0530

    adding one to array

---
+++ Q39.java
@@ -0,0 +1,33 @@
+import java.util.Collections;
+import java.util.Vector;
+
+public class Q39 {
+    
+    Vector<Integer> addOne(int[] arr){
+        Vector<Integer> ans = new Vector<>();
+        int n = arr.length;
+        int carry = 1;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]+carry<=9){
+                ans.add(arr[i]+carry);
+                carry = 0;
+            }
+            else{
+                ans.add(0);
+                carry = 1;
+            }
+        }
+        if(carry==1) ans.add(1);
+        Collections.reverse(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Q39 obj = new Q39();
+        int[] arr = {9, 9, 9};  // Example input
+        Vector<Integer> result = obj.addOne(arr);
+        System.out.println("Result: " + result);
+    }
+
+}
+
+
