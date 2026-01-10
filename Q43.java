1fa06326388eae10c44e3b955fd761c71701ed7e
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-10 21:10:50 +0530

    count vowels

---
+++ Q43.java
@@ -0,0 +1,13 @@
+public class Q43 {
+    public static void main(String[] args) {
+        String s = "ahewjsfbuiadajdaed";
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if( ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+                count++;
+        }
+        System.out.print(count);
+    }
+}
+
