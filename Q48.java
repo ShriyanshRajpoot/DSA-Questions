e5a11e6920f8046d8bab03341cb6bb927bcd54d7
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-12 18:35:01 +0530

    reverseb

---
+++ Q48.java
@@ -0,0 +1,20 @@
+public class Q48 {
+    public static void main(String[] args) {
+        String s = "Devesh";
+        StringBuilder sb = new StringBuilder(s);
+       // sb.reverse();  in built
+       // s = sb.toString();
+       // System.out.println(s);
+        int i = 0, j = sb.length()-1;
+        while(i<=j){
+            char temp1 = sb.charAt(i);
+            char temp2 = sb.charAt(j);
+            sb.setCharAt(i,temp2);
+            sb.setCharAt(j,temp1);
+            i++;
+            j--;
+        }
+         System.out.println(sb);
+    }
+}
+
