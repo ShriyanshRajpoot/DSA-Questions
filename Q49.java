f202ad51a9a0dd655c5249178c242de34c7ce431
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-13 21:20:35 +0530

    string to array

---
+++ Q49.java
@@ -0,0 +1,11 @@
+// stringtoarray
+public class Q49 {
+    public static void main(String[] args) {
+        String s = "Ruhan";
+        char[] arr = s.toCharArray();
+        for (char ch : arr) {
+            System.out.println(ch);
+        }
+    }
+}
+
