89d682ae2713be12dc59237031e21e7e966f6c5b
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-18 19:14:22 +0530

    constant time

---
+++ Q52.java
@@ -0,0 +1,9 @@
+public class Q52 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        // Accessing any element takes constant time
+        System.out.println(arr[2]); // O(1)
+    }
+}
+
+
