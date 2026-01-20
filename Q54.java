0f20fc9db1ef031bbc77e139bca2e46846cea376
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-01-20 22:22:14 +0530

    O(n)

---
+++ Q54.java
@@ -0,0 +1,9 @@
+public class Q54 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        // Traversing the array
+        for (int num : arr) {
+            System.out.print(num+" "); // O(n)
+        }
+    }
+}
